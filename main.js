a9c0f4f35ef0332588b496ca264b2f9b4a393e2d
CeciCode <101422795+CeciCode@users.noreply.github.com>
2022-09-23 17:58:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-var model= "https://teachablemachine.withgoogle.com/models/6zjdpU2lJ/"
+function sons() {
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier= ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/6zjdpU2lJ/model.json", modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {}
+}
